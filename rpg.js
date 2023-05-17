a04183b9be4a8ea8b7d1191792ddd65ce925ac86
seokgae <132544088+seokgae@users.noreply.github.com>
2023-05-17 14:37:13 +0900

    Add files via upload

--- rpg.js
+++ rpg.js
@@ -6,9 +6,9 @@ class Monster {
     }
 
     info() {
-        document.write("name: "+this.name+"\r");br();
-        document.write("hp: "+this.hp+"\r");br();
-        document.write("attack: "+this.attack+"\r");br();
+        document.write("name: "+this.name);br();
+        document.write("hp: "+this.hp);br();
+        document.write("attack: "+this.attack);br();
     }
 }
 
@@ -20,12 +20,29 @@ class Character {
     }
 
     info() {
-        document.write("name: "+this.name+'\n\n');br();
-        document.write("hp: "+this.hp+"\r");br();
-        document.write("attack: "+this.attack+"\r");br();
+        document.write("name: "+this.name);br();
+        document.write("hp: "+this.hp);br();
+        document.write("attack: "+this.attack);br();
     }
+}
+
+function Battle_turn(mon, cha) {
+    var mon_atk = RandomizeAttack(cha.attack); var cha_atk = RandomizeAttack(mon.attack);
+    mon.hp -= mon_atk;
+    cha.hp -= cha_atk;
+    document.write(cha.name+"이 "+mon.name+"에게 "+cha_atk+"의 피해를 입혔습니다."); br(); mon.info(); br(); 
+    document.write(mon.name+"이 "+cha.name+"에게 "+mon_atk+"의 피해를 입혔습니다."); br(); cha.info(); br();
+    hr();
 }
 
 const orc = new Monster('Orc', 120, 20);
 const elf = new Character('Elf', 100, 10);
 orc.info(); br(); elf.info();
+
+Battle_start();Battle_turn(orc, elf); Battle_turn(orc, elf);
+
+
+
+
+
+
